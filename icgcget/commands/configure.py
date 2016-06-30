2c329d3fed127dc33c83a7c3fc63cf0091f1b0b6
GavinMWilson <Gavin.M.Wilson@oicr.on.ca>
2016-06-30 15:56:48 -0400

    Increased configure verbosity

--- icgcget/commands/configure.py
+++ icgcget/commands/configure.py
@@ -39,47 +39,64 @@ class ConfigureDispatcher(object):
         print "You will receive a series of prompts for all relevant configuration values and access parameters "
         print "Existing configuration values are listed in square brackets.  To keep these values, press Enter."
         print "To input multiple values for a prompt, separate each value with a space."
+        print "Enter a directory on your machine for downloaded files to be saved to."
         output = self.prompt('output', 'output', input_type=click.Path(exists=True, writable=True, file_okay=False,
                                                                        resolve_path=True))
+        print "Enter a location for the process logs to be stored.  Must be in an existing directory.  Optional"
         logfile = self.prompt('logfile', 'logfile')
+        print "Enter which repositories you want to download from."
+        print "Valid repositories are: aws-virginia cghub collaboratory ega gdc pdc"
         repos = self.prompt('repos', 'repos')
         repos = repos.split(' ')
         validate_repos(repos, repo_list)
+        print "Enter if you wish to use a docker container to download and run all download clients"
         docker = self.prompt('docker', 'docker', input_type=click.BOOL)
         conf_yaml = {'output': output, 'logfile': logfile, 'repos': repos, 'docker': docker}
         if "aws-virginia" in repos or "collaboratory" in repos:
+            print "Enter the path to your local icgc storage client installation"
             icgc_path = self.prompt('ICGC path', 'icgc_path',
                                     input_type=click.Path(exists=True, dir_okay=False, resolve_path=True), skip=docker)
-            icgc_access = self.prompt('ICGC token', 'icgc_token', hide=True)
-            conf_yaml["icgc"] = {'path': icgc_path, 'token': icgc_access}
+            print "Enter a valid icgc access token"
+            icgc_access = self.prompt('ICGC token', 'icgc_token')
+            conf_yaml["icgc"] = {'token': icgc_access}
             if icgc_path:
                 conf_yaml['icgc']['path'] = icgc_path
         if "cghub" in repos:
+            print "Enter the path to your local genetorrent binaries"
             cghub_path = self.prompt('CGHub path', 'cghub_path', input_type=click.Path(exists=True, dir_okay=False,
                                                                                        resolve_path=True), skip=docker)
-            cghub_access = self.prompt('CGHub key', 'cghub_key', hide=True)
+            print "Enter a valid CGHub access key"
+            cghub_access = self.prompt('CGHub key', 'cghub_key')
             conf_yaml["cghub"] = {'key': cghub_access}
             if cghub_path:
                 conf_yaml['cghub']['path'] = cghub_path
         if "ega" in repos:
+            print "Enter the path to your local EGA download client jar file"
             ega_path = self.prompt('EGA path', 'ega_path',
                                    input_type=click.Path(exists=True, dir_okay=False, resolve_path=True), skip=docker)
+            print "Enter your EGA username"
             ega_username = self.prompt('EGA username', 'ega_username')
-            ega_password = self.prompt('EGA password', 'ega_password', hide=True)
-            conf_yaml["ega"] = {'path': ega_path, 'username': ega_username, 'password': ega_password}
+            print "Enter your EGA password"
+            ega_password = self.prompt('EGA password', 'ega_password')
+            conf_yaml["ega"] = {'username': ega_username, 'password': ega_password}
             if ega_path:
                 conf_yaml['ega']['path'] = ega_path
         if "gdc" in repos:
+            print "Enter the path to your local GDC download client installation"
             gdc_path = self.prompt('GDC path', 'gdc_path',
                                    input_type=click.Path(exists=True, dir_okay=False, resolve_path=True), skip=docker)
-            gdc_access = self.prompt('GDC token', 'gdc_token', hide=True)
+            print "Enter a valid GDC access token"
+            gdc_access = self.prompt('GDC token', 'gdc_token')
             conf_yaml["gdc"] = {'token': gdc_access}
             if gdc_path:
                 conf_yaml['gdc']['path'] = gdc_path
         if "pdc" in repos:
-            pdc_path = self.prompt('PDC path', 'pdc_path',
+            print "Enter the path to your local aws-cli installation to access the PDC repository"
+            pdc_path = self.prompt('AWS path', 'pdc_path',
                                    input_type=click.Path(exists=True, dir_okay=False, resolve_path=True), skip=docker)
+            print "Enter your PDC s3 key"
             pdc_key = self.prompt('PDC key', 'pdc_key')
+            print "Enter your PDC s3 secret key"
             pdc_secret_key = self.prompt('PDC secret key', 'pdc_secret', hide=True)
             conf_yaml['pdc'] = {'key': pdc_key, 'secret': pdc_secret_key}
             if pdc_path:
